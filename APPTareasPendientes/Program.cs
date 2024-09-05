b79d882a2895f51fec2f2a69db2546b32f898b3e
christian <christian_6366@hotmail.com>
2024-09-05 10:38:26 -0500

    frontend

--- APPTareasPendientes/Program.cs
+++ APPTareasPendientes/Program.cs
@@ -1,7 +1,22 @@
+using APPTareasPendientes.Models;
+using APPTareasPendientes.Services;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.Configure<AppSettingsModel>(builder.Configuration.GetSection("ApiSettings"));
+
+builder.Services.AddHttpClient<TareaService>((serviceProvider, client) =>
+{
+    var apiSettings = serviceProvider.GetRequiredService<IOptions<AppSettingsModel>>().Value;
+    client.BaseAddress = new Uri(apiSettings.BaseAddress);
+    client.DefaultRequestHeaders.Accept.Clear();
+    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+});
 
 var app = builder.Build();
 
@@ -18,6 +33,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Tarea}/{action=Index}/{id?}");
 
 app.Run();
